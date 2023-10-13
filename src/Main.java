5a48567e75b111edceb5683362139618f156ef18
PortalFennec <portal.foxian@gmail.com>
2023-10-13 13:31:52 +0300

    JavaCalc version 4

--- src/Main.java
+++ src/Main.java
@@ -1,125 +1,83 @@
 import java.util.Scanner;
+class Calc{
+    double num1, ans;
+    public static double Sum(double num1, double num2){
+        num1 = num1 + num2;
+        return num1;
+    }
+    public static double Diff(double num1, double num2){
+        num1 = num1 - num2;
+        return num1;
+    }
+    public static double Multiply(double num1, double num2){
+        num1 = num1 * num2;
+        return num1;
+    }
+    public static double Div(double num1, double num2){
+        num1 = num1 / num2;
+        return num1;
+    }
+}
+
 public class Main
-    {
+{
     public static void main(String[] args)
     {
-        double num1, num2, ans, ans2;
         boolean decision;
         char choose, con;
+        Calc Eval = new Calc();
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter num1 and num2: ");
-        num1 = reader.nextDouble();
-        num2 = reader.nextDouble();
-        System.out.print("\nEnter operator. Operators available (+, -, *, /): ");
-        choose = reader.next().charAt(0);
-        switch(choose){
-
-            //Sum
-            case '+': {
-            ans = num1 + num2;
-            System.out.print("\nSum of " + num1 + " and " + num2 + " = " + ans);
-            break;
-            }
-
-            //Diff
-            case '-': {
-                ans = num1 - num2;
-                System.out.print("\nDiff of " + num1 + " and " + num2 + " = " + ans);
-                break;
-            }
-
-            //Multiply
-            case '*': {
-                ans = num1 * num2;
-                System.out.print("\nMultiply of " + num1 + " and " + num2 + " = " + ans);
-                break;
-            }
-
-            //Div
-            case '/': {
-                ans = num1 / num2;
-                System.out.print("\nDiv of " + num1 + " and " + num2 + " = " + ans);
-                break;
-            }
-            default:  System.out.print("Error! Enter correct operator");
-                return;
-    }
-        System.out.print("\nContinue evaluation? y/n: ");
-        con = reader.next().charAt(0);
-        if (con == 'y') {
-            decision = true;
-            while (decision) {
-                System.out.print("\nEnter operator. Operators available (+, -, *, /): ");
-                choose = reader.next().charAt(0);
-                switch (choose) {
-
-                    //Sum
-                    case '+': {
-                        System.out.print("\nEnter the number with which you want to add: ");
-                        num2 = reader.nextDouble();
-                        ans2 = ans + num2;
-                        System.out.print("\nSum of " + ans + " and " + num2 + " = " + ans2);
-                        System.out.print("\nContinue evaluation? y/n: ");
-                        con = reader.next().charAt(0);
-                        ans = ans2;
-                        if (con == 'n') {
-                            decision = false;
-                        }
-                        break;
-                    }
-
-                    //Diff
-                    case '-': {
-                        System.out.print("\nEnter the number you want to subtract: ");
-                        num2 = reader.nextDouble();
-                        ans2 = ans - num2;
-                        System.out.print("\nDiff of " + ans + " and " + num2 + " = " + ans2);
-                        System.out.print("\nContinue evaluation? y/n: ");
-                        con = reader.next().charAt(0);
-                        if (con == 'n') {
-                            decision = false;
-                        }
-                        else {
-                            ans = ans2;
-                        }
-                        break;
-                    }
-
-                    //Multiply
-                    case '*': {
-                        System.out.print("\nEnter the number you want to multiply by: ");
-                        num2 = reader.nextDouble();
-                        ans2 = ans * num2;
-                        System.out.print("\nMultiply of " + ans + " and " + num2 + " = " + ans2);
-                        System.out.print("\nContinue evaluation? y/n: ");
-                        con = reader.next().charAt(0);
-                        if (con == 'n') {
-                            decision = false;
-                        }
-                        else {
-                            ans = ans2;
-                        }
-                        break;
-                    }
-
-                    //Div
-                    case '/': {
-                        System.out.print("\nEnter the number with which you want to add: ");
-                        num2 = reader.nextDouble();
-                        ans2 = ans / num2;
-                        System.out.print("\nDiv of " + ans + " and " + num2 + " = " + ans2);
-                        System.out.print("\nContinue evaluation? y/n: ");
-                        con = reader.next().charAt(0);
-                        if (con == 'n') {
-                            decision = false;
-                        }
-                        else {
-                            ans = ans2;
-                        }
-                        break;
-                    }
-                    }
+        System.out.print("Enter number you want operate with: ");
+        Eval.ans = reader.nextDouble();
+        decision = true;
+        while (decision)
+        {
+            System.out.print("\nEnter operator. Operators available (+, -, *, /): ");
+            choose = reader.next().charAt(0);
+            switch (choose)
+            {
+                //Sum
+                case '+': {
+                    System.out.print("\nEnter the number with which you want to add: ");
+                    Eval.num1 = reader.nextDouble();
+                    Eval.ans = Calc.Sum(Eval.ans, Eval.num1);
+                    System.out.print("\nResult: " + Eval.ans);
+                    break;
                 }
+
+                //Diff
+                case '-': {
+                    System.out.print("\nEnter the number you want to subtract: ");
+                    Eval.num1 = reader.nextDouble();
+                    Eval.ans = Calc.Diff(Eval.ans, Eval.num1);
+                    System.out.print("\nResult: " + Eval.ans);
+                    break;
+                }
+
+                //Multiply
+                case '*': {
+                    System.out.print("\nEnter the number you want to multiply by: ");
+                    Eval.num1 = reader.nextDouble();
+                    Eval.ans = Calc.Multiply(Eval.ans, Eval.num1);
+                    System.out.print("\nResult: " + Eval.ans);
+                    break;
+                }
+
+                //Div
+                case '/': {
+                    System.out.print("\nEnter the number with which you want to divide by: ");
+                    Eval.num1 = reader.nextDouble();
+                    Eval.ans = Calc.Div(Eval.ans, Eval.num1);
+                    System.out.print("\nResult: " + Eval.ans);
+                    break;
+                }
+                default:  System.out.print("Error! Enter correct operator");
+                    return;
             }
+            System.out.print("\nContinue evaluation? y/n: ");
+            con = reader.next().charAt(0);
+            if (con == 'n')
+                decision = false;
         }
-        }
+    }
+}
